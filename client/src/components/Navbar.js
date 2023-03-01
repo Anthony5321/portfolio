3b83a1e857a23611ae2011b2bb789ee5cfe40c95
anthony5321 <tyharpo12@gmail.com>
2023-02-28 22:09:12 -0500

    last commit for the night. cleaned up the navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { HashLink as Link } from "react-router-hash-link";
-import Home from "./Home";
-import About from "./About";
-import Contact from "./Contact";
-import Skills from "./Skills";
-import Work from "./Work";
+// import Home from "./Home";
+// import About from "./About";
+// import Contact from "./Contact";
+// import Skills from "./Skills";
+// import Work from "./Work";
+import "./NavbarStyles.css"
 
-const Navbar = () => {
+
+class Navbar extends Component {
+state={clicked: false}
+handleClick = () => {
+    this.setState({clicked: 
+        !this.state.clicked})
+}
+render(){
+
     return (
-        <section className="App-header" id= "Navbar">
+        <section className="navbar">
             <BrowserRouter>
-            <ul>
-    <li><a><Link to='#home' smooth>Home</Link></a></li>
+            <div className="navbar-links">
+            <ul id="navbar" className={this.state.clicked ? "#navbar active": "#navbar"}>
+    <li><a className="active"><Link to='#home' smooth>Home</Link></a></li>
     <li><a><Link to='#about' smooth>About</Link></a></li>
     <li><a><Link to='#skills' smooth>Skills</Link></a></li>
     <li><a><Link to='#work' smooth>Work</Link></a></li>
     <li><a><Link to='#contact' smooth>Contact</Link></a></li>
     </ul>
+    </div>
+    <div id="mobile" onClick={this.handleClick}>
+        <i id="bar"
+        className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+    </div>
     </BrowserRouter>
         </section>
     )
+}
 }
 
 export default Navbar;
