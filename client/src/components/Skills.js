3f89a8a44a72b853c63a8f21639a55e7403d189c
anthony5321 <tyharpo12@gmail.com>
2023-02-28 22:08:42 -0500

    navbar up and running

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -3,16 +3,16 @@ import React from "react";
 const Skills = () => {
     return (
         <section className="App-header" id= "skills">
-             <h1><span>Hey, I'm Anthony!</span> I'm a <span class="title">Avionics tech turned Programmer</span> and
-        <span class="container-flip">
-          <span class="flip flip-chef"> Skills chef.</span>
-          <span class="flip flip-air"> air-soft lover.</span>
-          <span class="flip flip-optimist"> optimist.</span>
-          <span class="flip flip-tinkerer"> tinkerer.</span>
-          <span class="flip flip-dog"> dog lover.</span>
-          <span class="flip flip-traveller"> traveller.</span>
-          <span class="flip flip-hiker"> hiker.</span>
-          <span class="flip flip-gamer"> gamer.</span>
+             <h1><span>Hey, I'm Anthony!</span> I'm a <span className="title">Avionics tech turned Programmer</span> and
+        <span className="container-flip">
+          <span className="flip flip-chef"> home chef.</span>
+          <span className="flip flip-air"> air-soft lover.</span>
+          <span className="flip flip-optimist"> optimist.</span>
+          <span className="flip flip-tinkerer"> tinkerer.</span>
+          <span className="flip flip-dog"> dog lover.</span>
+          <span className="flip flip-traveller"> traveller.</span>
+          <span className="flip flip-hiker"> hiker.</span>
+          <span className="flip flip-gamer"> gamer.</span>
         </span>
       </h1>
 
