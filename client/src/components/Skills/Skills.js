6d3a675fe2fe53277cdbf4a496973a77988bdd7c
anthony5321 <tyharpo12@gmail.com>
2023-03-04 00:51:20 -0500

    added some skills to the list

--- client/src/components/Skills/Skills.js
+++ client/src/components/Skills/Skills.js
@@ -1,22 +1,74 @@
 import React from "react";
+import './SkillsStyles.css';
+import {FaHtml5} from "react-icons/fa";
+import {DiCss3} from "react-icons/di";
+import {IoLogoJavascript} from "react-icons/io";
+import {FaReact} from "react-icons/fa";
+import {DiMongodb} from "react-icons/di";
+import {SiExpress} from "react-icons/si";
+import {SiPostgresql} from "react-icons/si";
+import {SiSequelize} from "react-icons/si";
 
 const Skills = () => {
     return (
-        <section className="App-header" id= "skills">
-             <h1><span>Hey, I'm Anthony!</span> I'm a <span className="title">Avionics tech turned Programmer</span> and
-        <span className="container-flip">
-          <span className="flip flip-chef"> home chef.</span>
-          <span className="flip flip-air"> air-soft lover.</span>
-          <span className="flip flip-optimist"> optimist.</span>
-          <span className="flip flip-tinkerer"> tinkerer.</span>
-          <span className="flip flip-dog"> dog lover.</span>
-          <span className="flip flip-traveller"> traveller.</span>
-          <span className="flip flip-hiker"> hiker.</span>
-          <span className="flip flip-gamer"> gamer.</span>
-        </span>
-      </h1>
-
-        </section>
+<section id="languages">
+    <div class="container">
+        <h1 class="text-center">Languages &amp; Frameworks</h1>
+        <div class="row">
+            <div class="col-lg-3 col-md-6 mt-4">
+                    <div class="card-body">
+                    <FaHtml5 className="skill-icon"/>
+                        <h4 class="card-title mt-3">HTML</h4>
+                    </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mt-4">
+                <div class="card-body">
+                <DiCss3 className="skill-icon"/>
+                    <h4 class="card-title mt-3">CSS</h4>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mt-4">
+                <div class="card-body">
+                   <IoLogoJavascript className="skill-icon"/>
+                    <h4 class="card-title mt-3">Javascript</h4>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mt-4">
+                <div class="card-body">
+                <FaReact className="skill-icon"/>
+                    <h4 class="card-title mt-3">React</h4>
+                </div>
+            </div>        
+        </div>
+        <div class="row">
+            <div class="col-lg-3 col-md-6 mt-4">
+                <div class="card-body">
+                <DiMongodb className="skill-icon"/>
+                    <h4 class="card-title mt-3">MongoDB</h4>
+                </div>
+            </div>
+            
+            <div class="col-lg-3 col-md-6 mt-4">
+                <div class="card-body">
+                <SiExpress className="skill-icon"/>
+                    <h4 class="card-title mt-3">Express</h4>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mt-4">
+                <div class="card-body">
+                <SiPostgresql className="skill-icon"/>
+                    <h4 class="card-title mt-3">SQL</h4>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mt-4">
+                <div class="card-body">
+                <SiSequelize className="skill-icon"/>
+                    <h4 class="card-title mt-3">Sequelize</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+  </section>
     )
 }
 
